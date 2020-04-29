29e2f6d1d24a2adc0af4e1c1af2c23f9dcfe1009
Pratiksha Shah <38119234+pratiksha-shah@users.noreply.github.com>
2020-04-28 17:41:42 -0700

    Update footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -16,15 +16,7 @@ const Footer = () => {
     <footer className={classes.footer}>
       <Typography variant="subtitle1" color="textSecondary" align="center">
         <Box display="flex" alignContent="center" justifyContent="center">
-          {"Made with "}
-          <FavoriteBorderIcon
-            color="secondary"
-            style={{ margin: "auto 0.25rem" }}
-          />
-          by&nbsp;
-          <Link color="inherit" href="https://stakater.com/">
-            Stakater
-          </Link>
+          {"Single source of truth "}
         </Box>
       </Typography>
     </footer>
